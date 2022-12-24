cdef69628ebd5949e87c4995ac1b9a6678771a52
Dimas Setya <dimassetya156@gmail.com>
2022-12-25 00:25:09 +0700

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
             <div class="header-body text-center mt-7 mb-7">
                 <div class="row justify-content-center">
                     <div class="col-lg-5 col-md-6">
-                        <h1 class="text-white">{{ __('Welcome to Argon Dashboard FREE Laravel Live Preview.') }}</h1>
+                        <h1 class="text-white">{{ __('Selamat Datang di Aplikasi Deteksi Lokasi Asset Berdasarkan Kode Inventaris.') }}</h1>
                     </div>
                 </div>
             </div>
